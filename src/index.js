d6b2bceb8e53b4d5568a93f2090d8fe563eebd9e
VENOMJ-P <jay63837@gmail.com>
2023-09-07 14:07:59 +0530

    Added tweet and hashtag logic

--- src/index.js
+++ src/index.js
@@ -9,20 +9,4 @@ app.listen(3000, async ()=>{
     console.log("Server started");
     connect();
     console.log("Successfully connected to mongodb")
-
-    // create a tweet with comment where UserEmail is not mandatory
-    const tweetRepository = new TweetRepository();
-    // const tweet = await tweetRepository.create({
-    //     content: "This is First tweet",
-    //     userEmail: "user1@google.com"
-    // });
-    // const comment = await Comment.create({
-    //     content: "This is Second comment of First tweet",
-    //     email:"user3@google.com"
-    // })
-    // tweet.comments.push(comment);
-    // await tweet.save();
-    const tweet = await tweetRepository.getWithComments("64f981217f2c40e48c3476fa")
-    
-    console.log(tweet);
 })
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -4,18 +4,16 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+        max: [250, "Tweet cannot be more than 250 characters"] // max: [range,error]
     },
-    userEmail: {
-        type: String
-    },
-    comments: [
+    hashtag: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
+            ref: 'Hashtag'
         }
     ]
 }, {
-    timestamps: true
+    timestamps: true //This helps to give createdAt and updatedAt
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
