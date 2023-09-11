e1409cdac90e774111ab63c8f843bf5c6e51873c
VENOMJ-P <jay63837@gmail.com>
2023-09-11 11:01:57 +0530

    Added like and user model and add crud repository

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,36 +1,12 @@
 import Tweet from '../models/tweet.js';
 
 class TweetRepository {
-    async create(data) {
-        try {
-            const tweet = await Tweet.create(data);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
+    constructor(){
+        super(Tweet)
     }
-
-    async get(tweetId) {
-        try {
-            const tweet = await Tweet.findById(tweetId);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     async getWithComments(tweetId) {
         try {
             const tweet = await Tweet.findById(tweetId).populate({ path: 'comments' }).lean();
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async destroy(tweetId) {
-        try {
-            const tweet = await Tweet.findByIdAndDelete(tweetId);
             return tweet;
         } catch (error) {
             console.log(error);
