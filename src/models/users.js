1f57c3b6eaa3845a708e56e7f3148aa0a57a3d08
VENOMJ-P <jay63837@gmail.com>
2023-09-13 19:12:08 +0530

    Added password encrypted user signup logic

--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -18,6 +19,15 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  const saltRounds = 9;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const encryptedPassword = bcrypt.hashSync(user.password, salt);
+  user.password = encryptedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createTweet, getTweet} from "../../controllers/tweet-controller.js";
 import { toggleLike } from "../../controllers/like-controller.js";
 import { createComment  } from "../../controllers/comment-controller.js";
+import { signup } from "../../controllers/user-controller.js";
 const router = express.Router();
 
 router.post("/tweets", createTweet);
@@ -9,5 +10,7 @@ router.get("/tweets/:id", getTweet);
 
 router.post("/likes/toggle",toggleLike);
 router.post("/comments",createComment);
+
+router.post("/signup",signup);
 
 export default router;
